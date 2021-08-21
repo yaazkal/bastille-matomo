93b04edc35cb9eca2b2b8d68dbe4e06d716e8f99
Yaazkal <yaazkal@gmail.com>
2021-08-20 21:39:09 -0500

    Automates mysql_secure_installation

--- root/bootstrap_matomo.sh
+++ root/bootstrap_matomo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Downlads matomo
+# Copyright © 2021 Juan David Hurtado G <jdhurtado@orbiware.com>
+
+# Downloads matomo
 echo "[INFO] Downloading matomo"
 fetch -q -o /tmp/matomo.zip https://builds.matomo.org/matomo.zip
 # Unzip the file
@@ -7,5 +9,20 @@ echo "[INFO] Extracting matomo"
 unzip -q -u -d /usr/local/www /tmp/matomo.zip
 # Set permissions
 chown -R www:www /usr/local/www/matomo
+
+# Ensure MariaDB runs securely -- @yaazkal
+if [ ! -e /root/.my.pass ]; then
+    echo "[INFO] Securing MariaDB"
+    yes | mysql_secure_installation >/dev/null 2>&1
+    RANDOM_PASSWORD=$(openssl rand -base64 15)
+    mysql -u root -pyes -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${RANDOM_PASSWORD}';"
+    cat << EOF > /root/.my.cnf
+[client]
+user=root
+password=${RANDOM_PASSWORD}
+EOF
+    echo "[INFO] Check /root/.my.cnf for the root password of the database server if needed"
+fi
+
 # Cleanup
 rm /root/bootstrap_matomo.sh
